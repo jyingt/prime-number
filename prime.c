9613bd698d081a38819699dd3dd580f95b2d0a7e
John Ying <tienchii@gmail.com>
2014-01-27 18:36:19 -0500

    Added user input feature

--- prime.c
+++ prime.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 // determine if number is a prime number
 int isPrime(int num)
 {
-    // check from 2..n^(1/2)
-    for (int i = 2; i <= sqrt(num); i++)
-        if (num % i == 0)
+    int ii;
+
+    // check if number is even
+    if (num % 2 == 0)
+        return 0;
+
+    // check divisibility by odd numbers from 3..n^(1/2)
+    for (ii = 3; ii <= sqrt(num); ii += 2)
+        if (num % ii == 0)
             return 0;
 
     return 1;
@@ -23,8 +30,24 @@ int nextPrimeNumber(int num)
 // unit test
 int main(int argc, char **argv)
 {
-    for (int i = 0; i < 50; i++) {
+    int i, num, count;
+    char buffer[256];
+
+    if (argc == 3) {
+        num = atoi(argv[1]);
+        count = atoi(argv[2]);
+    }
+    else {
+        printf("Enter starting number: ");
+        fgets(buffer, 256, stdin);
+        num = strtol(buffer, NULL, 10);
+
+        printf("Enter # of primes to list: ");
+        fgets(buffer, 256, stdin);
+        count = strtol(buffer, NULL, 10);
+    }
+
+    for (i = 0; i < count; i++) {
         printf("Prime number after %d is %d.\n", i, nextPrimeNumber(i));
-        //printf("%d: %d, %d\n", i, isPrime(i), nextPrimeNumber(i));
     }
 }
